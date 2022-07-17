e54da28d14324fc8e403fe7d6d4b49410a48c98c
Artur Logan <arturlogan1ferreira@gmail.com>
2022-07-17 19:45:04 -0300

    Projeto inicial spring

---
+++ src/main/java/com/artur/cadastroorcamento/CadastroorcamentoApplication.java
@@ -0,0 +1,13 @@
+package com.artur.cadastroorcamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroorcamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroorcamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artur/cadastroorcamento/CadastroorcamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artur.cadastroorcamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroorcamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
